7445a173fd76ae41366d32acabbcd1297b2d9532
Caio Stadelhofer Dias <35613252+CaioStadelhoferDias@users.noreply.github.com>
2020-02-04 10:43:17 -0300

    Tela de Campanha

--- src/pages/campanha/diasCampanha/dataDiasCampanha.js
+++ src/pages/campanha/diasCampanha/dataDiasCampanha.js
@@ -9,7 +9,7 @@ function random() {
 
 export function generateData(count) {
   var i;
-  var dia = ['Dia 1', 'Dia  2', 'Dia 3', 'Dia 4', 'Dia 5', 'Dia 6'];
+  var dia = ['Dia 1', 'Dia 2', 'Dia 3', 'Dia 4', 'Dia 5', 'Dia 6'];
   var meta = ['50000', '100000', '75000', '175000', '90000', '45000'];
   var duracao = ['45','30','31','20','365','90'];
   var discount = ['1', '0.15', '0.20', '0.05', '0.08', '0.07'];
