7a6a0e7da18118dab33f102a79e70cc4adc56546
Bruno Luan Weber <bruno@mederis.com.br>
2020-02-25 18:06:57 -0300

    Pacote Draggable adicionado

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,22 +3,36 @@ import './home.scss';
 import { ChartBar } from '../../components';
 import { dataGrafico } from '../../data';
 import { Grid } from '@material-ui/core';
+import Draggable from 'react-draggable';
 
 export default () => (
   <React.Fragment>
-    <Grid container spacing={1}>
-      <Grid item xs={12} md={6}>
-        <ChartBar type="bar" data={dataGrafico} field="mes" />
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <ChartBar type="line" data={dataGrafico} field="mes" />
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <ChartBar type="spline" data={dataGrafico} field="mes" />
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <ChartBar type="fullstackedbar" data={dataGrafico} field="mes" />
-      </Grid>
+    <Grid spacing={6}>
+
+      <Draggable bounds="body">
+        <Grid className="box cursor" item xs={12} md={6}>
+          <ChartBar type="bar" data={dataGrafico} field="mes" />
+        </Grid>
+      </Draggable>
+
+      <Draggable bounds="body">
+        <Grid className="box cursor" item xs={12} md={6}>
+          <ChartBar type="line" data={dataGrafico} field="mes" />
+        </Grid>
+      </Draggable>
+
+      <Draggable bounds="body">
+        <Grid className="box cursor" item xs={12} md={6}>
+          <ChartBar type="spline" data={dataGrafico} field="mes" />
+        </Grid>
+      </Draggable>
+
+      <Draggable bounds="body">
+        <Grid className="box cursor" item xs={12} md={6}>
+          <ChartBar type="fullstackedbar" data={dataGrafico} field="mes" />
+        </Grid>
+      </Draggable>
+
     </Grid>
   </React.Fragment>
 );
